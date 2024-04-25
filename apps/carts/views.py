fa3a6990586538ba784843b49ef647927cebebb8
Toktorov <ktoktorov2005@gmail.com>
2024-04-25 15:26:22 +0600

    add api for cart and cart item

--- apps/carts/views.py
+++ apps/carts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
-# Create your views here.
+from apps.carts.models import Cart,CartItem
+from apps.carts.serializers import CartSerializer,CartItemSerializer
+
+class CartViewSet(ListCreateAPIView):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
+
+class CartDetailAPI(RetrieveUpdateDestroyAPIView):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
+    
+class CartItemViewSet(ListCreateAPIView):
+    queryset = CartItem.objects.all()
+    serializer_class = CartItemSerializer
+
+class CartItemDetailAPI(RetrieveUpdateDestroyAPIView):
+    queryset = CartItem.objects.all()
+    serializer_class = CartItemSerializer
+    
