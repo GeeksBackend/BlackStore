8f014fe6ee123551487b3c188b9ee5247fd8fc61
Toktorov <ktoktorov2005@gmail.com>
2024-05-06 15:58:14 +0600

    add jwt tokens and IsAuthenticated to product review

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,5 +1,6 @@
 from rest_framework.viewsets import GenericViewSet
 from rest_framework import mixins
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from apps.products.models import Product
 from apps.products.serializers import ProductSerializer
@@ -12,3 +13,8 @@ class ProductAPI(GenericViewSet,
                  mixins.DestroyModelMixin):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    def get_permissions(self):
+        if self.action == 'retrieve':
+            return (IsAuthenticated(), )
+        return (AllowAny(), )
